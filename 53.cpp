00e77cb0c9715a4d0c96c1e594f138fb46394824
M A Alim <alim1496@gmail.com>
2022-10-10 00:41:03 +0600

    Create 53.cpp

---
+++ 53.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int max_sum = INT_MIN;
+        int current_sum = 0;
+        
+        for(int n:nums) {
+            current_sum = max(n, current_sum+n);
+            max_sum = max(current_sum, max_sum);
+        }
+        
+        return max_sum;
+    }
+};
