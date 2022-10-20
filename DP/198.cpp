c39b767cbdfbd06801aa2cb48441f6fb851fd758
M A Alim <alim1496@gmail.com>
2022-10-21 02:55:17 +0600

    Update 198.cpp

--- DP/198.cpp
+++ DP/198.cpp
@@ -22,7 +22,6 @@ public:
         int dp[n+1];
         dp[n] = 0;
         for(int i=nums.size()-1;i>=0;i--) {
-            int current = i;
             int sum = nums[i];
             if(i+2 < n) sum += dp[i+2];
             dp[i] = max(sum, dp[i + 1]);
