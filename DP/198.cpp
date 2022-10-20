c6bafb865037af65e4a8576436a07b5f20373878
M A Alim <alim1496@gmail.com>
2022-10-21 02:30:08 +0600

    Update 198.cpp

--- DP/198.cpp
+++ DP/198.cpp
@@ -1,22 +1,16 @@
 // memoised recursion
 class Solution {
 public:
-    int dp[101];
-    int helper(vector<int>& nums, int house) {
+    int helper(vector<int>& nums, vector<int>& dp, int house) {
         if(house >= nums.size()) return 0;
         if(dp[house] != -1) return dp[house];
         
-        int current = house;
-        int sum = 0;
-        sum += nums[current];
-        current += 2;
-        
-        return dp[house] = max(sum + helper(nums, current), helper(nums, house + 1));
+        return dp[house] = max(nums[house] + helper(nums, dp, house+2), helper(nums, dp, house + 1));
     }
     
     int rob(vector<int>& nums) {
-        memset(dp, -1, sizeof dp);
-        return helper(nums, 0);
+        vector<int> dp(nums.size(), -1);
+        return helper(nums, dp, 0);
     }
 };
 
@@ -24,14 +18,13 @@ public:
 class Solution {
 public:
     int rob(vector<int>& nums) {
-        int dp[nums.size()+1];
-        dp[nums.size()] = 0;
+        int n = nums.size();
+        int dp[n+1];
+        dp[n] = 0;
         for(int i=nums.size()-1;i>=0;i--) {
             int current = i;
-            int sum = 0;
-            sum += nums[current];
-            current += 2;
-            if(current < nums.size()) sum += dp[current];
+            int sum = nums[i];
+            if(i+2 < n) sum += dp[i+2];
             dp[i] = max(sum, dp[i + 1]);
         }
         return dp[0];
