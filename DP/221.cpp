51a1b74d1a3eed8861c436dc09502788eec48008
M A Alim <alim1496@gmail.com>
2022-10-24 13:56:48 +0600

    Create 221.cpp

---
+++ DP/221.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int maximalSquare(vector<vector<char>>& matrix) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        vector<vector<int>> dp(m, vector<int>(n));
+        int ms = 0;
+        
+        for(int i=0;i<m;i++) {
+            for(int j=0;j<n;j++) {
+                if(i == 0 || j == 0) {
+                    dp[i][j] = matrix[i][j]-'0';
+                } else if(matrix[i][j] == '1') {
+                    dp[i][j] = min(min(dp[i-1][j], dp[i][j-1]), dp[i-1][j-1]) + 1;
+                }
+                ms = max(ms, dp[i][j]);
+            }
+        }
+        
+        return ms*ms;
+    }
+};
